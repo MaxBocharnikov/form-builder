97beb3c17f60a96b8743c23c7de661ea2525345d
Maksim Bocharnikov <mbocharnikov@alfabank.ru>
2023-03-19 00:10:38 +0400

    add some styling to the form + button

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import FormBuilder from '../../components/builder'
+import Button from '../../components/ui-components/button'
 import { FormField, FormResult } from '../../types/builder'
 
 import styles from './index.module.css'
@@ -31,14 +32,32 @@ const fieldsSchema: FormField[] = [
   }
 ]
 const Main: React.FC = () => {
+  const [formResult, setFormResult] = useState<FormResult | null>(null)
+
+  const onSubmit = () => {
+    console.log('result: ', formResult)
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <FormBuilder
-        fields={fieldsSchema}
-        onChangeHandler={(result: FormResult) => {
-          console.log(result)
-        }}
-      />
+    <div className={styles.container}>
+      <div className={styles.formWrapper}>
+        <h2 className={styles.title}>Авторизация</h2>
+        <p className={styles.text}>
+          Для доступа к личному кабинету вашей компании авторизуйстесь на сайте
+        </p>
+        <FormBuilder
+          fields={fieldsSchema}
+          onChangeHandler={(result: FormResult) => { setFormResult(result) }}
+        />
+        <Button
+          text="Войти"
+          type="submit"
+          view="primary"
+          onClick={onSubmit}
+          className={styles.submit}
+          disabled={!formResult?.isValid}
+        />
+      </div>
     </div>
   )
 }
